aca4416b5c1e8a62cf4ac95e1e940ce3503cc939
Nacho-Fenix <129918631+Nacho-Fenix@users.noreply.github.com>
2023-06-24 18:51:41 -0300

    Update TopMusical.java

--- TopMusical.java
+++ TopMusical.java
@@ -4,70 +4,103 @@ import java.util.Scanner;
 
 public class TopMusical {
 
-    // Mapa de bandas y sus canciones populares
-    private static Map<String, String[]> topBandas = new LinkedHashMap<>();
+        // Mapa de bandas y sus canciones populares
+        private static Map<String, String[]> topBandas = new LinkedHashMap<>();
 
-    public static void main(String[] args){
-        
-    // Solicita al usuario el top que quiere
+        public static void main(String[] args) {
+                // Agregamos los tops de música predefinidos
+                agregarTopsPredefinidos();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Bienvenido a este Top 10 de las mejores bandas de Rock internacional");
-        System.out.print("Las bandas del top son:");
-        System.out.print("\nTop 1:Theory of Deadman\nTop 2:Foo Fighters\nTop 3:Linking Park\nTop 4:Pearl Jam\nTop 5:TOTO\nTop 6:Green Day\nTop 7:Good Charlotte\nTop 8:System of a Down\nTop 9:Creed\nTop 10:Thirty Seconds To Mars\n\n");
-        System.out.print("Ingresa el top que quieres (1-10):");
-        int top = scanner.nextInt();
-        scanner.close();
+                //Solicitamos al usuario el top que quiere ver y a su vez con un loop para preguntarle si quiere ver otro top
+                Scanner scanner = new Scanner(System.in);
+                boolean continuar = true;
 
-        // Analiza si el top ingresado es válido
-        if (top < 1 || top > 10) {
-            System.out.println("El top ingresado no es válido.");
-            return;
+                while (continuar) {
+                        int top;
+                        
+                        do {
+                                System.out.println();
+                                System.out.println("Bienvenido a este Top 10 de las mejores bandas de Rock internacional");
+                                System.out.print("Las bandas del top son:");
+                                System.out.println();
+                                System.out.print("\nTop 1:Theory of Deadman\nTop 2:Foo Fighters\nTop 3:Linking Park\nTop 4:Pearl Jam\nTop 5:TOTO\nTop 6:Green Day\nTop 7:Good Charlotte\nTop 8:System of a Down\nTop 9:Creed\nTop 10:Thirty Seconds To Mars\n\n");
+                                System.out.print("Ingresa el top que quieres ver (1-10): ");
+                                top = scanner.nextInt();
+
+                                // Aca se verifica que el numero ingresado sea correcto (1-10)
+                                if (top < 1 || top > 10) {
+                                        System.out.println();
+                                        System.out.println("¡El número del top no es válido!");
+                                        System.out.println("Ingrese nuevamente.");
+                                }
+                        } while (top < 1 || top > 10);
+
+                        // Obtenemos la lista de bandas y canciones del top
+                        String[] bandas = topBandas.keySet().toArray(new String[0]);
+                        String banda = bandas[top - 1];
+                        String[] canciones = topBandas.get(banda);
+
+                        // Mostrara las canciones del top luego que el usuario ingrese el numero correspondiente
+                        System.out.println();
+                        System.out.println("Top " + top + " Banda: " + banda);
+                        for (int i = 0; i < canciones.length; i++) {
+                                System.out.println((i + 1) + "º" + canciones[i]);
+                        }
+
+                        // Aca le preguntamos al usuario si quiere ingresar otro top
+                        System.out.println();
+                        System.out.print("¿Deseas ingresar otro top? (s/n): ");
+                        String opcion = scanner.next();
+                        continuar = opcion.equalsIgnoreCase("s");//Lo usamos para ignorar la diferencia ente mayuscula y minuscula al ingresar (s/n)
+                }
+
+                scanner.close();
+
+                // Mensaje final de despedida
+                System.out.println("!Gracias por su tiempo, esperamos le sea de ayuda esta informacion!");
         }
-        // Se invoca a la lista de canciones
-        agregarTopsPredefinidos();
 
-        // Obtener la lista de bandas y canciones del top
-        String[] bandas = topBandas.keySet().toArray(new String[0]);
-        String banda = bandas[top - 1];
-        String[] canciones = topBandas.get(banda);
+        // Agregar tops de musica predefinidos al mapa
+        private static void agregarTopsPredefinidos() {
+                // top1
+                topBandas.put("Theory of Deadman", new String[] { "Canción:Point to prove", "Canción:The Last song",
+                                "Canción:By the Way", "Canción:Two of us", "Canción:Angel" });
+                // top2
+                topBandas.put("Foo Fighters", new String[] { "Canción:Best of you", "Canción:We run", "Canción:Walk",
+                                "Canción:The pretender", "Canción:long" });
+                // top3
+                topBandas.put("Linking Park", new String[] { "Canción:In the end", "Canción:Numb",
+                                "Canción:Leave it out the rest", "Canción:Papercut", "Canción:Castle of glass" });
+                // top4
+                topBandas.put("Pearl Jam", new String[] { "Canción:EvenFlow", "Canción:Alive", "Canción:Black",
+                                "Canción:Soldier of Love", "Canción:Animal" });
+                // top5
+                topBandas.put("TOTO",
+                                new String[] { "Canción:Can you hear what im saying", "Canción:Love has the power",
+                                                "Canción:Tale of a man", "Canción:The chains",
+                                                "Canción:Dont chain my heart" });
+                // top6
+                topBandas.put("Green Day",
+                                new String[] { "Canción:Basket Case", "Canción:American Idiot", "Canción:21 Guns",
+                                                "Canción:Boulevard of Broken Dreams", "Canción:Holiday" });
+                // top7
+                topBandas.put("Good Charlotte", new String[] { "Canción:I Just Wanna Live", "Canción:We Believe",
+                                "Canción:The River", "Canción:The Chronicles of Life and Death",
+                                "Canción:Last December" });
+                // top8
+                topBandas.put("System of a Down",
+                                new String[] { "Canción:Chop Suey", "Canción:Aerials", "Canción:Question",
+                                                "Canción:Genocidal Humanoidz", "Canción:Deer Dance" });
+                // top9
+                topBandas.put("Creed", new String[] { "Canción:My Sacrifice", "Canción:One Last Breath",
+                                "Canción:Dont's Top Dancing", "Canción:My Own Prison", "Canción:With Arms Wide Open" });
+                // top10
+                topBandas.put("Thirty Seconds To Mars",
+                                new String[] { "Canción:Edge Of The Earth", "Canción:Capricorn (A Brand New Name)",
+                                                "Canción:Oblivion",
+                                                "Canción:Echelon", "Canción:The Kill (Bury Me)" });
 
-        // Mostrar las canciones del top
-        System.out.println("Top " + top + " Banda: " + banda);
-        for (int i = 0; i < canciones.length; i++) {
-            System.out.println((i + 1) + "º" + canciones[i]);
+                // Agregar mas bandas y canciones aca...
+                /* AGREGAR MAS INTERACCIONES CON EL USUARIO */
         }
-    }
-
-    // Agregar tops de musica predefinidos al mapa
-    private static void agregarTopsPredefinidos() {
-        // top1
-        topBandas.put("Theory of Deadman", new String[] { "Canción:Point to prove", "Canción:The Last song",
-                "Canción:By the Way", "Canción:Two of us", "Canción:Angel" });
-        // top2
-        topBandas.put("Foo Fighters", new String[] { "Canción:Best of you", "Canción:We run", "Canción:Walk",
-                "Canción:The pretender", "Canción:long" });
-        // top3
-        topBandas.put("Linking Park", new String[] { "Canción:In the end", "Canción:Numb",
-                "Canción:Leave it out the rest", "Canción:Papercut", "Canción:Castle of glass" });
-        // top4
-        topBandas.put("Pearl Jam", new String[] { "Canción:EvenFlow", "Canción:Alive", "Canción:Black",
-                "Canción:Soldier of Love", "Canción:Animal" });
-        // top5
-        topBandas.put("TOTO", new String[] { "Canción:Can you hear what im saying", "Canción:Love has the power",
-                "Canción:Tale of a man", "Canción:The chains", "Canción:Dont chain my heart" });
-        // top6
-        topBandas.put("Green Day", new String[] { "Canción:Basket Case", "Canción:American Idiot", "Canción:21 Guns", "Canción:Boulevard of Broken Dreams", "Canción:Holiday" });
-        // top7
-        topBandas.put("Good Charlotte", new String[] { "Canción:I Just Wanna Live", "Canción:We Believe", "Canción:The River", "Canción:The Chronicles of Life and Death", "Canción:Last December" });
-        // top8
-        topBandas.put("System of a Down", new String[] { "Canción:Chop Suey", "Canción:Aerials", "Canción:Question", "Canción:Genocidal Humanoidz", "Canción:Deer Dance" });
-        // top9
-        topBandas.put("Creed", new String[] { "Canción:My Sacrifice", "Canción:One Last Breath", "Canción:Dont's Top Dancing", "Canción:My Own Prison", "Canción:With Arms Wide Open" });
-        // top10
-        topBandas.put("Thirty Seconds To Mars", new String[] { "Canción:Edge Of The Earth", "Canción:Capricorn (A Brand New Name)", "Canción:Oblivion", "Canción:Echelon", "Canción:The Kill (Bury Me)" });
-
-        // Agregar mas bandas y canciones aca...
-        /*AGREGAR MAS INTERACCIONES CON EL USUARIO Y CREAR TOPS EN EL CUAL EL USUARIO CREE LOS TOPS*/
-    }
 }
